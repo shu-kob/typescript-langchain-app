974738cae43e5817516b9fbf7bfe7305c642463d
shu-kob <shukobuchi+github@gmail.com>
2024-05-05 17:14:05 +0900

    RAGを追加

--- backend/src/chat.ts
+++ backend/src/chat.ts
@@ -1,5 +1,6 @@
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai'
 import { HarmBlockThreshold, HarmCategory } from '@google/generative-ai'
+import { v1beta, type protos } from '@google-cloud/discoveryengine'
 
 /*
  * Before running this, you should make sure you have created a
@@ -24,12 +25,56 @@ const model = new ChatGoogleGenerativeAI({
 })
 
 export const chat = async (input: string): Promise<any> => {
+  const context: string = await getDocumentsFromRAG(input)
+
   // Batch and stream are also supported
   const res = await model.invoke([
     [
-      "human",
+      'system',
+      `${context}を元にして質問に答えます。`
+    ],
+    [
+      'human',
       input
     ]
   ])
   return res.content
+}
+
+export const getDocumentsFromRAG = async (input: string): Promise<string> =>  {
+  let responseContent: string = '' // 初期化
+  const dataSourceId: string = process.env.VERTEXAI_DATASOURCE_ID ?? ''
+  const apiEndpointFromEnv: string = 'discoveryengine.googleapis.com'
+  const servingConfigId: string = 'default_config'
+  // Instantiates a client
+  const client = new v1beta.SearchServiceClient({ apiEndpoint: apiEndpointFromEnv })
+  const request = {
+    servingConfig: dataSourceId + '/servingConfigs/' + servingConfigId,
+    query: input,
+    pageSize: 10,
+    queryExpansionSpec: { condition: 'AUTO' },
+    spellCorrectionSpec: { mode: 'AUTO' },
+    contentSearchSpec: {
+      summarySpec: {
+        summaryResultCount: 5
+      }
+    }
+  } as protos.google.cloud.discoveryengine.v1beta.SearchRequest
+
+  const IResponseParams = {
+    ISearchResult: 0,
+    ISearchRequest: 1,
+    ISearchResponse: 2
+  }
+
+  // Perform search request
+  const response = await client.search(request, {
+    autoPaginate: false
+  })
+  const { summary, results } = response[IResponseParams.ISearchResponse] as protos.google.cloud.discoveryengine.v1beta.ISearchResponse
+
+  responseContent = summary?.summaryText ?? ''
+  console.log('RAG Context:')
+  console.log(responseContent)
+  return responseContent
 }
